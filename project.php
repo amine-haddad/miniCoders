8cb71308e6521d44851ae3e82c6ea8cbfe7e49f5
Olivier3231 <maffrolivier74@gmail.com>
2021-03-19 00:23:09 +0100

    htmland css done

--- project.php
+++ project.php
@@ -1,1 +1,50 @@
-//Ce sera mon fichier de travail Put your Code here!!
+
+<link rel="stylesheet" href="../assets/style.css">
+
+
+
+<body class=backgroundProject >
+ <section class=project>
+
+    <header class=fixedHeader>
+    <!--Placer header-->
+    </header>
+        
+        <main class=content>
+            <div class=underProject>
+                <h2>Rencontrez des Dev</h2>
+                <a href="https://zupimages.net/viewer.php?id=21/11/sr2r.jpg"><img src="https://zupimages.net/up/21/11/sr2r.jpg" alt="" /></a>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Laudantium voluptates recusandae cupiditate eos, quos, quis suscipit adipisci natus inventore delectus architecto repellat quod cum corrupti!
+                     Natus, consequuntur dignissimos? Ratione, rem! Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                     Alias natus aliquam aut, recusandae distinctio mollitia nam in dignissimos tempora aliquid, a dolorum magni incidunt minus?
+                      Eaque corrupti pariatur nostrum hic.</p>
+            </div>
+
+            <div class=underProject>
+                <h2>Voyages</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                     Nesciunt fugiat ex, est recusandae dicta veniam optio quia harum natus ipsam totam.
+                      Similique autem, cumque ullam non obcaecati debitis et nostrum! Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      Ad repellendus, laborum nesciunt earum repudiandae eligendi animi possimus illum perferendis architecto voluptatum ea eos qui ipsam ab deserunt corrupti iusto minima.</p>
+                <a href="https://zupimages.net/viewer.php?id=21/11/xiq3.jpg"><img src="https://zupimages.net/up/21/11/xiq3.jpg" alt="" /></a>
+            </div>
+
+            <div class=underproject>
+                <h2>La Bretagne vous gagne</h2>
+                <a href="https://zupimages.net/viewer.php?id=21/11/a25u.jpg"><img src="https://zupimages.net/up/21/11/a25u.jpg" alt="" /></a>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non qui magnam animi quaerat est, quasi natus voluptas itaque adipisci reiciendis quidem aut sequi rem.
+                     Ut porro accusamus nisi labore maxime! Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                     Impedit provident, recusandae officiis nulla, excepturi, molestiae commodi dolore totam eos beatae tenetur minima sunt rerum.
+                      Voluptatum culpa eos ab laborum natus?</p>
+            </div> 
+        </main>    
+            <aside class=socialMedia>
+                <!--side bar-->
+            </aside> 
+    <footer class=fixedFooter>
+        <!--put footer here-->
+    </footer>    
+ </section>
+
+</body>;
