2da4dcea335d8205228112d44d4528a6ee950f0e
fourneau <cathyjulien14@orange.fr>
2021-03-19 16:52:53 +0100

    css final pour contact4

--- contact.php
+++ contact.php
@@ -3,15 +3,16 @@
 <section id="contact" class="all_contact">
     <h6>Contactez John DOE</h6>
         <p>Merci de remplir le formulaire et John DOE vous répondra</p>
-            <form class=" formulaire ">
-                <label for="name">Nom de Famille:</label><br>
-                <input type="text" id="name" name="fname" value="DOE"><br>
-                <label for="last name">Prénom:</label><br>
-                <input type="text" id="last name" name="lname" value="John"><br>
-                <label for="email">Email:</label><br>
-                <input type="email" id="email" name="email" value="johnDOE@LancelotduLac.com"><br>
-                <label for="message">Message:</label><br>
-                <textarea id="message" name="message"></textarea><br><br>
-                <button class="btn" type="button">SOUMETTRE</button>
+        <div class="container">
+                <form class=" formulaire ">
+                    <label for="name">Nom de Famille:</label><br>
+                    <input type="text" id="name" name="fname" value="DOE"><br>
+                    <label for="last name">Prénom:</label><br>
+                    <input type="text" id="last name" name="lname" value="John"><br>
+                    <label for="email">Email:</label><br>
+                    <input type="email" id="email" name="email" value="johnDOE@LancelotduLac.com"><br>
+                    <label for="message">Message:</label><br>
+                    <textarea id="message" name="message"></textarea><br><br>
+                    <button class="btn" type="button">SOUMETTRE</button>
             </form>
 </section>
