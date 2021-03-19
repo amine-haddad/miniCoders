267e3b80d6aceffc2374fb1e1516d786efa744fa
fourneau <cathyjulien14@orange.fr>
2021-03-19 17:29:44 +0100

    index opérationnel

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 
-<?php include _header.php?>
-<?php include home.php?>
-<?php include aboutMe.php?>
-<?php include project.php?>
-<?php include contact.php?>
-<?php include _footer.php?>
+<?php include "_layout/_header.php"?>
+<?php include "home.php"?>
+<?php include "aboutMe.php"?>
+<?php include "project.php"?>
+<?php include "contact.php"?>
+<?php include "_layout/_footer.php"?>
