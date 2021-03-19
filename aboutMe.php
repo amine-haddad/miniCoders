08e8853c64e4e68f6cb344391554fa8731ea1f23
Anne <ga.annegren@gmail.com>
2021-03-19 10:36:06 +0100

    udpdatem

--- aboutMe.php
+++ aboutMe.php
@@ -1,3 +1,17 @@
+<link rel="stylesheet" href="../assets/style.css">
+
 <section>
+    
+<h5>ABOUT ME</h5>
+
+<ul class="flex-container">
+    
+  <li class="flex-item"> <img src="https://zupimages.net/up/21/11/jsy7.jpg" alt="mon portrait" width="550"></li>
+
+  <li class="flex-item">Hello I am John Doe. 
+   <p>Auxerunt haec vulgi sordidioris audaciam, quod cum ingravesceret penuria commeatuum, famis et furoris inpulsu Eubuli cuiusdam inter suos clari domum ambitiosam ignibus subditis inflammavit rectoremque ut sibi iudicio imperiali addictum calcibus incessens et pugnis conculcans seminecem laniatu miserando discerpsit. post cuius lacrimosum interitum in unius exitio quisque imaginem periculi sui considerans documento recenti similia formidabat.</p>
+  </li>
+
+</ul>
 
 </section>
