84af71c338048ee20b0fcf602dd8efea0f16f4cc
amine-haddad <seniortouco@outlook.fr>
2021-03-19 18:10:51 +0100

    modif header et footer et style css

--- _layout/_header.php
+++ _layout/_header.php
@@ -12,13 +12,13 @@
 <section class=containerheader>
      
 <div id="burger">
-     <!--<i class="fa fa-bars fa-2x" aria-hidden="true"></i>-->
+     <i class="fa fa-bars fa-2x" aria-hidden="true"></i>
      <input type="checkbox" id="burger_checkbox"/>
       <ul class=menubuttons>
-            <li class=buttonHome><a href="#" class=btnHrefNav >Home</a></li>
-            <li class=buttonAbout><a href="#" class=btnHrefNav>About Me</a></li>
-            <li class=buttonProject><a href="#"class=btnHrefNav>Project</a></li>
-            <li class=buttonContact><a href="#"class=btnHrefNav>Contact</a></li>
+            <li class=buttonHome><a href="home.php" class=btnHrefNav >Home</a></li>
+            <li class=buttonAbout><a href="aboutMe.php" class=btnHrefNav>About Me</a></li>
+            <li class=buttonProject><a href="project.php"class=btnHrefNav>Project</a></li>
+            <li class=buttonContact><a href="contact.php"class=btnHrefNav>Contact</a></li>
         </ul>   
      </div>   
      
