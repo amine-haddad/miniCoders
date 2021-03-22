8d8a814f522bb55c7aaaef6e39e21196ccc75554
fourneau <cathyjulien14@orange.fr>
2021-03-22 12:11:59 +0100

    contact final branche définitif

--- contact.php
+++ contact.php
@@ -4,14 +4,14 @@
         <p class="Merci">Merci de remplir le formulaire et John DOE vous répondra</p>
                     <form class=" formulaire ">
                     <label for="name">Nom de Famille:</label><br>
-                    <input type="text" id="name" name="fname" value="DOE"><br>
+                    <input class="enterFormulaire" type="text" id="name" name="fname" value="DOE"><br>
                     <label for="last name">Prénom:</label><br>
-                    <input type="text" id="last name" name="lname" value="John"><br>
+                    <input class="enterFormulaire" type="text" id="last name" name="lname" value="John"><br>
                     <label for="email">Email:</label><br>
-                    <input type="email" id="email" name="email" value="johnDOE@LancelotduLac.com"><br>
+                    <input class="enterFormulaire" type="email" id="email" name="email" value="johnDOE@LancelotduLac.com"><br>
                     <label for="message">Message:</label><br>
-                    <textarea id="message" name="message"></textarea><br><br>
-                    <button class="btn" type="button">SOUMETTRE</button>
+                    <textarea class="texteClient" id="message" name="message"></textarea><br><br>
+                    <button class="btnContact" type="button">SOUMETTRE</button>
             </form>
 </section>
 
