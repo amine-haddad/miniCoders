c6ee09086532558cc75b6e09bc6aaa358d7ffc4f
fourneau <cathyjulien14@orange.fr>
2021-03-24 19:12:26 +0100

    contact a vendre2

--- contact.php
+++ contact.php
@@ -4,11 +4,11 @@
                     
                 <form class="formulaire" id="form">
                     <label for="name">Lastname:</label><br>
-                    <input class="enterFormulaire" type="text" id="name" name="fname" value="DOE"><br>
+                    <input class="enterFormulaire" type="text" id="name" name="fname" value="DOE" onfocus= "javascript : this.value= ''"><br>
                     <label for="last name">Firstname:</label><br>
-                    <input class="enterFormulaire" type="text" id="last name" name="lname" value="John"><br>
+                    <input class="enterFormulaire" type="text" id="last name" name="lname" value="John" onfocus= "javascript : this.value= ''"><br>
                     <label for="email">Email:</label><br>
-                    <input class="enterFormulaire" type="email" id="email" name="email" value="johnDOE@LancelotduLac.com"><br>
+                    <input class="enterFormulaire" type="email" id="email" name="email" value="johnDOE@LancelotduLac.com" onfocus= "javascript : this.value= ''"><br>
                     <label for="message">Message:</label><br>
                     <textarea class="texteClient" id="message" name="message"></textarea><br><br>
                     <div class="sectionSubmit"><button class="btnContact" type="submit"><strong>Send</strong></button></div>
