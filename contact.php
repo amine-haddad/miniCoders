fa545993d323603152839508d00a28640dd13522
fourneau <cathyjulien14@orange.fr>
2021-03-24 15:26:15 +0100

    contact a vendre

--- contact.php
+++ contact.php
@@ -1,8 +1,8 @@
 <section id="contact" class="all_contact">
-    <h2 class="animate-text">CONTACT  JOHN  DOE</h2>
+    <h2 class="animate-text">CONTACT</h2>
         <p class="Merci">Please fill in the form and John Doe will answer you</p>
                     
-                <form class=" formulaire ">
+                <form class="formulaire" id="form">
                     <label for="name">Lastname:</label><br>
                     <input class="enterFormulaire" type="text" id="name" name="fname" value="DOE"><br>
                     <label for="last name">Firstname:</label><br>
@@ -11,7 +11,7 @@
                     <input class="enterFormulaire" type="email" id="email" name="email" value="johnDOE@LancelotduLac.com"><br>
                     <label for="message">Message:</label><br>
                     <textarea class="texteClient" id="message" name="message"></textarea><br><br>
-                    <div class="sectionSubmit"><button class="btnContact" type="button"><strong>Send</strong></button></div>
+                    <div class="sectionSubmit"><button class="btnContact" type="submit"><strong>Send</strong></button></div>
                     
             </form>
         </div>
