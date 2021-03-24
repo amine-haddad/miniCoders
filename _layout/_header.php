55e3858642a70e8186e69c67161f814e1a747585
amine-haddad <seniortouco@outlook.fr>
2021-03-24 21:04:40 +0100

    new header

--- _layout/_header.php
+++ _layout/_header.php
@@ -9,26 +9,36 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet"> 
+    <script src="https://kit.fontawesome.com/12c728ad22.js" crossorigin="anonymous"></script>
 
 </head>
 <body id="home_page_id">
 <section class=containerheader>
 
+<header class=headerNormal>
     <div class="nameSignature" id="signatureJohn">
             <h1 class=signedJohn>John doe</h1>
-    </div>
-     
-    <div id="burger">
-    
-     <input class="checkboxInput" type="checkbox" id="burger_checkbox"/>
-     <label for="burger_checkbox"></label>
-        <ul class=menubuttons id="menu">
-            <li class ="btnNav" id = "buttonHome"><a href="#home_page"  class="aNavhf" id="btnHrefNav" >Home</a></li>
-            <li class ="btnNav" id = "buttonAbout"><a href="#pageAboutMe"  class="aNavhf" id="btnHrefNav">About Me</a></li>
-            <li class ="btnNav" id = "buttonProject"><a href="#pageProject" class="aNavhf" id="btnHrefNav">Project</a></li>
-            <li class ="btnNav" id = "buttonContact"><a href="#contact" class="aNavhf" id="btnHrefNav">Contact</a></li>
-        </ul>   
-   
-</div>     
+        </div>
+        <nav id="burger">
+            <ul class="btnUl">
+                <li class ="btnNav" id = "buttonHome"><a href="#home_page"  class="aNavhf" id="btnHrefNav" >Home</a></li>
+                <li class ="btnNav" id = "buttonAbout"><a href="#pageAboutMe"  class="aNavhf" id="btnHrefNav">About Me</a></li>
+                <li class ="btnNav" id = "buttonProject"><a href="#pageProject" class="aNavhf" id="btnHrefNav">Project</a></li>
+                <li class ="btnNav" id = "buttonContact"><a href="#contact" class="aNavhf" id="btnHrefNav">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="navBurger">
+        <a href="index.php" class="active">John Doe</a>
+        <div id="myLinks">
+            <a href="#home_page"  class="aNavhf" id="btnHrefNav" >Home</a>
+            <a href="#pageAboutMe"  class="aNavhf" id="btnHrefNav">About Me</a>
+            <a href="#pageProject" class="aNavhf" id="btnHrefNav">Project</a>
+            <a href="#contact" class="aNavhf" id="btnHrefNav">Contact</a>
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="burgerMenu()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>   
 
 </section>
--- assets/main.js
+++ assets/main.js
@@ -55,3 +55,17 @@ function animate_home_text() {
 animate_home_text();
 
 
+//partie js header
+function burgerMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+      x.style.display = "none";
+  } else {
+      x.style.display = "block";
+  }
+}
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
