ff0302484d3e6f8f6e80abf16471af539f71557d
fourneau <cathyjulien14@orange.fr>
2021-03-25 17:40:03 +0100

    contact en attente

--- contact.php
+++ contact.php
@@ -1,18 +1,16 @@
 <section id="contact" class="all_contact">
     <h2 class="animate-text">CONTACT</h2>
-        <p class="Merci">Please fill in the form and John Doe will answer you</p>
-                    
-                <form class="formulaire" id="form">
-                    <label for="name">Lastname:</label><br>
-                    <input class="enterFormulaire" type="text" id="name" name="fname" value="DOE" onfocus= "javascript : this.value= ''"><br>
-                    <label for="last name">Firstname:</label><br>
-                    <input class="enterFormulaire" type="text" id="last name" name="lname" value="John" onfocus= "javascript : this.value= ''"><br>
-                    <label for="email">Email:</label><br>
-                    <input class="enterFormulaire" type="email" id="email" name="email" value="johnDOE@LancelotduLac.com" onfocus= "javascript : this.value= ''"><br>
-                    <label for="message">Message:</label><br>
-                    <textarea class="texteClient" id="message" name="message"></textarea><br><br>
-                    <div class="sectionSubmit"><button class="btnContact" type="submit"><strong>Send</strong></button></div>
-                    
-            </form>
-        </div>
+        <p class="Merci">Please fill in the form and John Doe will answer you</p>  
+
+        <form method="POST" action="" class="formulaire" id="form">
+            <label for="name">Lastname:</label><br>
+            <input class="enterFormulaire" type="text" id="name" name="fname" value="DOE" onfocus= "javascript : this.value= ''"><br>
+            <label for="last name">Firstname:</label><br>
+            <input class="enterFormulaire" type="text" id="last name" name="lname" value="John" onfocus= "javascript : this.value= ''"><br>
+            <label for="email">Email:</label><br>
+            <input class="enterFormulaire" type="email" id="email" name="email" value="johnDOE@LancelotduLac.com" onfocus= "javascript : this.value= ''"><br>
+            <label for="message">Message:</label><br>
+            <textarea class="texteClient" id="message" name="message"></textarea><br><br>
+            <div class="sectionSubmit"><button class="btnContact" type="submit"><strong>Send</strong></button></div>
+        </form>  
 </section>
